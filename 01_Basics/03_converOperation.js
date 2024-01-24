5fe14314993c54556784950acbea4be7748d2f58
Sunni-Kayal-12 <141425978+Sunni-Kayal-12@users.noreply.github.com>
2024-01-24 15:29:27 +0000

    conversion of datatype

---
+++ 01_Basics/03_converOperation.js
@@ -0,0 +1,23 @@
+//let score= "33ad"
+//let score= "33"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumb =Number(score)
+//console.log(typeof valueInNumb);
+//console.log(valueInNumb); //Nan means not a number 
+//Nan is special type
+
+//"33" => 33
+//"23dd" => Nan
+//true =>1 false=0
+
+//let isLoggedIn =1
+//let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+let someNum =16
+
+let stringNum=String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);
