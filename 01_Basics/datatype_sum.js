1122d5ea6bb103724049e3f1d3c105e8b5c2702e
Sunni-Kayal-12 <141425978+Sunni-Kayal-12@users.noreply.github.com>
2024-01-24 18:28:19 +0000

    datatypesum

---
+++ 01_Basics/datatype_sum.js
@@ -0,0 +1,22 @@
+const sc=100 //js is dynamic 
+const scoVal = 100.3
+
+const isLoggedIn = false
+const outsideTemo =null
+let userEmail; 
+
+const id=Symbol('222')
+const anothId= Symbol('222')
+console.log(id === anothId);
+
+const bigNumb =9993931877124534n
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
