8e79b3e102b07f4e57b60ac4ffda66f7de3826db
Sunni-Kayal-12 <141425978+Sunni-Kayal-12@users.noreply.github.com>
2024-01-24 15:03:50 +0000

    dtatype

---
+++ 01_Basics/02_dtatype.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+//primitivedata types
+
+//alert(3+3) innodejs we avoid ; (semi-colon)
+//we are using nodejs, not browser
+//number=>2 to power 53
+//we cannot assign null. null is spefied value 
+//like let state =null //its wrong to define //its empty value
+//bigint
+//string =>  ""
+// null => standalone value
+//undefined =>abhi value assign nhi hui hai
+//symbol =>unique
+//object
+
+//what is null? ans) Null is an object.
+
+console.log(typeof "sunaina");
+console.log(typeof null);
